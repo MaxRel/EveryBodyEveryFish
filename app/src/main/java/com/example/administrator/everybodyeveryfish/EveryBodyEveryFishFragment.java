43e1c60d83f52fbfff04bb77fc1e02bd3d8bb55a
Themaxmaxmax <34407316+MaxRel@users.noreply.github.com>
2019-03-16 18:27:34 +0800

    demo

---
+++ app/src/main/java/com/example/administrator/everybodyeveryfish/EveryBodyEveryFishFragment.java
@@ -0,0 +1,22 @@
+package com.example.administrator.everybodyeveryfish;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class EveryBodyEveryFishFragment extends Fragment {
+
+    public EveryBodyEveryFishFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_every_body_every_fish, container, false);
+    }
+}
